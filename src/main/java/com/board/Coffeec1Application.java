4b4b74b342774753ad2b0a7668bca87c0ab878e3
wnsho <jhkimm00@naver.com>
2021-08-13 00:35:59 +0900

    프로젝트 등록

---
+++ src/main/java/com/board/Coffeec1Application.java
@@ -0,0 +1,13 @@
+package com.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coffeec1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coffeec1Application.class, args);
+	}
+
+}
